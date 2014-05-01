02343f8099d5b7788b2b39677be4ea1e65d0afdc
David <david@gmail.com>
2014-05-01 12:56:47 -0600

    Refactoring

--- src/util/yt_core_defs.php
+++ src/util/yt_core_defs.php
@@ -4,6 +4,8 @@ $lt_11i = "k"
 $lt_26h = "e"
 $analt_p29 = strrev(".xaoh etarobale nA");
 $fatr_i630a = strrev(" ytraF") . "is " . strrev(" laer a") . "human being.";
-$splr_63q = "I'm flattered that you took it seriously, though :P";
+$splr_qqa = "I'm flattered that you ";
+$splr_qqb = "took it seriously, though :P";
+$splr_63q = $splr_qqa . $splr_qqb;
 
 ?>
